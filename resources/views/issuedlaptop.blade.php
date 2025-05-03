445e73a1f4b0ec593f773547c17ddf4befba67e0
sankars1997 <111554844+sankars1997@users.noreply.github.com>
2025-05-03 16:34:39 +0530

    Update issuedlaptop.blade.php

--- resources/views/issuedlaptop.blade.php
+++ resources/views/issuedlaptop.blade.php
@@ -1,1 +1,39 @@
-@extends('layout.app')
+<div>@extends('layout.app')</div>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <div >
+
+
+    </div>
+</head>
+
+<div class="container">
+<body>
+
+    <table>
+        <caption>Laptop List</caption>
+        <thead>
+            <tr>
+                
+                <th>Serial No</th>
+                
+                <th>Specifications</th>
+                
+            </tr>
+        </thead>
+        <tbody>
+        @foreach ($laptops as $laptop )
+            <tr>
+                <td>{{ $laptop->Serial_no }}</td>
+                <td>{{ $laptop->specifications }}</td>
+                
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+   
+</body>
+</div>
+</html>

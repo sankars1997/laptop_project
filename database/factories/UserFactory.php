59c218d7ae75661fd26cb512e0cbcea2ae233d2f
sankars1997 <111554844+sankars1997@users.noreply.github.com>
2025-05-03 16:31:37 +0530

    Update UserFactory.php

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,5 +1,3 @@
-<?php
-
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
@@ -24,10 +22,11 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-           
-            
-           
-            
+            'name' => fake()->name(),
+            'email' => fake()->unique()->safeEmail(),
+            'email_verified_at' => now(),
+            'password' => static::$password ??= Hash::make('password'),
+            'remember_token' => Str::random(10),
         ];
     }
 

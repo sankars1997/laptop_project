739921303828999ccd6bb88ab360898478943aa6
sankars1997 <111554844+sankars1997@users.noreply.github.com>
2025-05-03 16:26:51 +0530

    Update FrontendController.php

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -8,9 +8,20 @@ Use App\Models\Laptop;
 class FrontendController extends Controller
 {
     
-public function InstockPage(){
-    $laptops=Laptop::all();
+public function Instock(){
+    $laptops = Laptop::where('active', 1)->latest()->limit(10)->get();
+
+   // $laptops=Laptop::where('active',0);
     return view('instock',compact('laptops'));
 }
+
+
+public function Issuedlaptop(){
+    $laptops = Laptop::where('active', 0)->latest()->limit(10)->get();
+
+   // $laptops=Laptop::where('active',0);
+    return view('instock',compact('laptops'));
+}
+
 
 }
